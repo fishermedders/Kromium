193e06bcb220bf79c432439e06835bec2402d7f1
Fisher <fishermedders@gmail.com>
2021-02-28 07:56:00 -0500

    First Commit

---
+++ build/webpack.base.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+
+const translateEnvToMode = (env) => {
+  if (env === "production") {
+    return "production";
+  }
+  return "development";
+};
+
+module.exports = env => {
+  return {
+    target: "electron-renderer",
+    mode: translateEnvToMode(env),
+    node: {
+      __dirname: false,
+      __filename: false
+    },
+    externals: [nodeExternals()],
+    resolve: {
+      alias: {
+        env: path.resolve(__dirname, `../config/env_${env}.json`)
+      }
+    },
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"]
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        }, {
+          test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        }, {
+          test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "url-loader?limit=10000&mimetype=application/octet-stream"
+        }, {
+          test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "url-loader?limit=10000&mimetype=application/vnd.ms-fontobject"
+        }, {
+          test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "url-loader?limit=10000&mimetype=image/svg+xml"
+        }
+      ]
+    },
+    plugins: [
+      new FriendlyErrorsWebpackPlugin({ clearConsole: env === "development" })
+    ]
+  };
+};
